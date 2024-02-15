2dce5f0b80b2cd965f45243a0a1b185d4c2ac6b8
Andrew Njoo <adnjoo@gmail.com>
2024-02-15 07:36:10 -0800

    draggable

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ import { DnD } from "@/components/DnD";
 
 export default async function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center p-24 min-h-screen bg-slate-500">
       <DnD />
     </main>
   );
--- web/src/components/DnD.tsx
+++ web/src/components/DnD.tsx
@@ -1,26 +1,42 @@
 "use client";
 
 import React from "react";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import Card from "@/components/Card";
+import Draggable from "react-draggable";
 
 export const DnD = () => {
   const [isDragging, setIsDragging] = React.useState(false);
-  const [className, setClassName] = React.useState("bg-gradient-to-r from-purple-500 to-indigo-500 p-8 rounded-xl shadow-lg text-white");
+  const [className, setClassName] = React.useState(
+    "bg-gradient-to-r from-purple-500 to-indigo-500 p-8 rounded-xl shadow-lg text-white"
+    // ""
+  );
   const [text, setText] = React.useState("Drag me");
   return (
     <>
-      <DndProvider backend={HTML5Backend}>
-        <Card text={text} isDragging={isDragging} className={className} />
-      </DndProvider>
-      <span className='mt-12'>Class name</span>
-      <div className="border mb-12">
-        <input className='h-[100px] w-[600px]' type="text" value={className} onChange={(e) => setClassName(e.target.value)} />
-      </div>
-      <span>Text</span>
-      <div className="border mb-12">
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      <Draggable>
+        <div className={className}>{text}</div>
+      </Draggable>
+      <div>
+        <div>
+          <span className="mt-12">Class name</span>
+          <div className="border mb-12">
+            <textarea
+              className="h-[100px] w-[600px]"
+              value={className}
+              onChange={(e) => setClassName(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <span>Text</span>
+          <div className="border mb-12">
+            <input
+              className="w-full"
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
